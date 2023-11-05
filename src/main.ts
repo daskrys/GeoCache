ad3d403513d0f4caa31621fd23abd4888d257116
daskrys <caperezl@ucsc.edu>
2023-11-04 18:31:54 -0700

    player can now collect and deposit

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,24 @@ import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
 
-const MERRILL_CLASSROOM = leaflet.latLng({
+const MERRILL_CLASSROOM: leaflet.LatLng = leaflet.latLng({
   lat: 36.9995,
   lng: -122.0533,
 });
 
-const GAMEPLAY_ZOOM_LEVEL = 18.5;
-const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const PIT_SPAWN_PROBABILITY = 0.1;
+let playerLatLang: leaflet.LatLng = leaflet.latLng({
+  lat: 36.9995,
+  lng: -122.0533,
+});
 
-const mapContainer = document.querySelector<HTMLElement>("#map")!;
+let clicked: boolean = false; // boolean to check if sensor button has been clicked but not needed for now
+
+const GAMEPLAY_ZOOM_LEVEL: number = 18.5;
+const TILE_DEGREES: number = 1e-4;
+const NEIGHBORHOOD_SIZE: number = 8;
+const PIT_SPAWN_PROBABILITY: number = 0.1;
+
+const mapContainer: HTMLElement = document.querySelector<HTMLElement>("#map")!;
 
 const map = leaflet.map(mapContainer, {
   center: MERRILL_CLASSROOM,
@@ -33,61 +40,112 @@ leaflet
   })
   .addTo(map);
 
-const playerMarker = leaflet.marker(MERRILL_CLASSROOM);
+const playerMarker: leaflet.Marker<any> = leaflet.marker(MERRILL_CLASSROOM);
 playerMarker.bindTooltip("That's you!");
 playerMarker.addTo(map);
 
-const sensorButton = document.querySelector("#sensor")!;
+const sensorButton: Element = document.querySelector("#sensor")!;
+
 sensorButton.addEventListener("click", () => {
-  navigator.geolocation.watchPosition((position) => {
+  let watchId: number = navigator.geolocation.watchPosition((position) => {
     playerMarker.setLatLng(
       leaflet.latLng(position.coords.latitude, position.coords.longitude)
     );
+    navigator.geolocation.clearWatch(watchId);
     map.setView(playerMarker.getLatLng());
+    playerLatLang = playerMarker.getLatLng();
+
+    createPits();
+    //updatePosition(); // commented out for now
   });
 });
 
-let points = 0;
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+let points: number = 0;
+const statusPanel: HTMLDivElement =
+  document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
 function makePit(i: number, j: number) {
   const bounds = leaflet.latLngBounds([
     [
-      MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + j * TILE_DEGREES,
+      playerLatLang.lat + i * TILE_DEGREES,
+      playerLatLang.lng + j * TILE_DEGREES,
     ],
     [
-      MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-      MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES,
+      playerLatLang.lat + (i + 1) * TILE_DEGREES,
+      playerLatLang.lng + (j + 1) * TILE_DEGREES,
     ],
   ]);
 
   const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-
-  pit.bindPopup(() => {
+  let pit1 = pit.bindPopup(() => {
     let value = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
     const container = document.createElement("div");
     container.innerHTML = `
                 <div>There is a pit here at "${i},${j}". It has value <span id="value">${value}</span>.</div>
-                <button id="poke">poke</button>`;
+                <button id="poke">poke</button>
+                <button id="deposit">deposit</button>
+                <p id="playerLocation">playerLocation</p>`;
     const poke = container.querySelector<HTMLButtonElement>("#poke")!;
+    const deposit = container.querySelector<HTMLButtonElement>("#deposit")!;
+    const playerLocation =
+      container.querySelector<HTMLParagraphElement>("#playerLocation")!;
+
     poke.addEventListener("click", () => {
-      value--;
+      if (value > 0) {
+        value--;
+        points++;
+      }
+
       container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
         value.toString();
-      points++;
       statusPanel.innerHTML = `${points} points accumulated`;
     });
+
+    deposit.addEventListener("click", () => {
+      if (points > 0) {
+        points--;
+        value++;
+      }
+
+      container.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+        value.toString();
+      statusPanel.innerHTML = `${points} points accumulated`;
+    });
+
+    container.querySelector<HTMLSpanElement>(
+      "#playerLocation"
+    )!.innerHTML = `Player Location: ${playerLatLang.toString()}`;
+
     return container;
   });
+
   pit.addTo(map);
 }
 
-for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
-  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
-    if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
-      makePit(i, j);
+function createPits() {
+  for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+    for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+      if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+        console.log(luck([i, j].toString()));
+        makePit(i, j);
+      }
     }
   }
 }
+// recursive function that updates position without generating new pits every time
+function updatePosition() {
+  setTimeout(() => {
+    playerMarker.setLatLng(playerLatLang);
+    map.setView(playerMarker.getLatLng());
+    console.log("Position Updated");
+    console.log(playerLatLang);
+    updatePosition();
+  }, 5000);
+}
+
+if (clicked) {
+  updatePosition();
+}
+
+createPits();
